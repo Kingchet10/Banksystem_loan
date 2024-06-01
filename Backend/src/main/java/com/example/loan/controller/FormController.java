557ca0e7f3105fc973bbecf40ed4803a6472f94d
konsilver <139610986+konsilver@users.noreply.github.com>
2024-06-01 17:56:20 +0800

    Update FormController.java

--- Backend/src/main/java/com/example/loan/controller/FormController.java
+++ Backend/src/main/java/com/example/loan/controller/FormController.java
@@ -1,13 +1,12 @@
 package com.example.loan.controller;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.loan.mapper.FormMapper;
 import entity.Form;
-import entity.Loan;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,7 +19,7 @@ public class FormController {
     private FormMapper formMapper;
 
     @PostMapping("/add-form")
-    public Map<String, Object> insertManager(@RequestBody Form form) {
+    public Map<String, Object> insertForm(@RequestBody Form form) {
         int result = formMapper.insert(form);
         Map<String, Object> response = new HashMap<>();
         if (result > 0) {
@@ -30,16 +29,5 @@ public class FormController {
             response.put("message", "Failed to create form.");
         }
         return response;
-    }
-    @GetMapping("/get-forms/{form_id}")
-    public IPage getForms(@PathVariable("form_id") int form_id,@RequestParam int page, @RequestParam int pageSize) {
-        Page<Form> formPage = new Page<>(page, pageSize);
-        return formMapper.selectPage(formPage, new QueryWrapper<Form>().eq("form_id", form_id));
-    }
-
-    @GetMapping("/search-froms/{id_number}")
-    public IPage searchForms(@PathVariable("id_number") String id_number, @RequestParam int page, @RequestParam int pageSize) {
-        Page<Form> formPage = new Page<>(page, pageSize);
-        return formMapper.selectPage(formPage, new QueryWrapper<Form>().eq("id_number", id_number));
     }
 }
