c5e4880dc440145e48986bebffc353004d37bb16
EeeMaoY <my1634325882@163.com>
2024-06-05 13:45:16 +0800

    Backend_Officer

--- Backend_Officer/src/main/java/com/example/loan/controller/FormController.java
+++ Backend_Officer/src/main/java/com/example/loan/controller/FormController.java
@@ -8,9 +8,6 @@ import entity.Form;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 @CrossOrigin
 public class FormController {
@@ -18,23 +15,9 @@ public class FormController {
     @Autowired
     private FormMapper formMapper;
 
-    @PostMapping("/add-form")
-    public Map<String, Object> insertForm(@RequestBody Form form) {
-        int result = formMapper.insert(form);
-        Map<String, Object> response = new HashMap<>();
-        if (result > 0) {
-            response.put("message", "Form created successfully!");
-            response.put("id", form.getForm_id()); // 返回生成的表单ID
-        } else {
-            response.put("message", "Failed to create form.");
-        }
-        return response;
-    }
-
     @GetMapping("/get-forms/{form_id}")
-    public IPage getForms(@PathVariable("form_id") int form_id, @RequestParam int page, @RequestParam int pageSize) {
-        Page<Form> formPage = new Page<>(page, pageSize);
-        return formMapper.selectPage(formPage, new QueryWrapper<Form>().eq("form_id", form_id));
+    public Form getFormDetails(@PathVariable("form_id") int form_id) {
+        return formMapper.selectById(form_id);
     }
 
     @GetMapping("/search-forms/{id_number}")
